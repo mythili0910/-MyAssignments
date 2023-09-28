0e85f0da758929044facb457f940964f3d9522ac
mythili0910 <145953330+mythili0910@users.noreply.github.com>
2023-09-28 10:08:54 +0530

    Add files via upload

---
+++ day3/Browser.java
@@ -0,0 +1,15 @@
+package week1.day3;
+
+public class Browser {
+
+	public String launchBrowser(String browserName) {
+		System.out.println(browserName+" Browser launched Successfully ");
+		return browserName;
+	}
+	public void loadUrl() {
+		System.out.println("Application loaded Successfully ");
+			}
+	
+	}
+
+
